52fc89a40b28a744daae16d2625fd41e79552b3f
Ryan <RyanSkurkis@gmail.com>
2014-07-17 18:15:33 -0500

    random test code

--- server.js
+++ server.js
@@ -1,10 +1,54 @@
 var express = require('express'),
+	_ = require('lodash'),
 	app = express();
 
+
+var fruits = [
+	{
+		name: "apple",
+		count: 53
+	},
+	{
+		name: "peach",
+		count: 500
+	},
+	{
+		name: "orange",
+		count: 50
+	}
+];
+
 app.get('/fruitcount', function(req, res){
-  res.json({
-  	count: 5
-  });
+	var fruitName = req.query.name,
+		fruit = null;
+
+	fruit = _.find(fruits, function(fruit){
+		return fruit.name === fruitName;
+	});
+
+	var results = _.chain(fruits)
+	.filter(function(fruit){
+		return fruit.count > 50;
+	})
+	.map(function(fruit){
+		return {
+			displayText: "The count for this fruit is " + fruit.count
+		};
+	})
+	.value();
+
+	if(fruit){
+		res.json({
+			count: fruit.count,
+			otherStuff: results
+		});
+		return;
+	}
+
+	res.json({
+		error: true,
+		message: "Fruit not found"
+	});
 });
 
 app.listen(3000);
